f6900c897038372d467e83942bf3df1fd146db75
trung-hieu <hieutran235@gmail.com>
2022-08-17 09:12:43 +0700

    create UI list of todo and test with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './App.css'
 
 import { StoreContext } from './store'
 import { useContext } from 'react'
+import TodoList from './components/UI/organisms/list/TodoList'
 
 function App() {
   const {state, dispatch} = useContext(StoreContext)
   console.log(state);
   
   return (
-   <></>
+   <>
+      <TodoList></TodoList>
+   </>
   )
 }
 
---
+++ src/components/mock-data/data.ts
@@ -0,0 +1,22 @@
+export const Data: Array<string> = [
+    'Racing car sprays burning fuel into crowd. Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.Racing car sprays burning fuel into crowd.',
+    'Australian walks 100km after outback crash.Racing car sprays burning fuel into crowd.',
+    'Man charg',
+    'Los Angel.',
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    
+  ];
